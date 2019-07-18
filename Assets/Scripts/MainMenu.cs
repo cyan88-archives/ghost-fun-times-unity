286e59a478fba678c624b3071f253753bcd62ef0
mollyyan <42104993+mollyyan@users.noreply.github.com>
2019-07-18 10:34:18 -0400

    wrong code

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,6 +4,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame(){
+        SceneManagement.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
+
+
+
+/* using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
 public class MainMenu : MonoBehaviour {
 
  public void PlayGame () 
@@ -19,3 +33,4 @@ public class MainMenu : MonoBehaviour {
   
 }
  
+*/
